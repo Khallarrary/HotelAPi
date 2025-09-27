2e9ef2d34170fd789fed7b322fc1711f5bf54128
José Aparecido Fernandes Junior <joseap.fer@hotmail.com>
2025-09-27 02:29:18 -0300

    👍

--- HotelSolicitacoesAPI/HotelSolicitacoesAPI/obj/Release/net8.0/HotelSolicitacoesAPI.AssemblyInfo.cs
+++ HotelSolicitacoesAPI/HotelSolicitacoesAPI/obj/Release/net8.0/HotelSolicitacoesAPI.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelSolicitacoesAPI")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4062eae7980263ede945ad10e2f87cfcc9ae55e")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelSolicitacoesAPI")]
